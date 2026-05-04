03531db7f21d1547d106dabbdae1c4982c338c76
Joudalsamari <445052056@STD.PSAU.EDU.SA>
2026-05-04 04:26:35 +0300

    add semaphore

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -41,7 +41,7 @@ class SharedResources {
     public static final java.util.concurrent.locks.ReentrantLock lock = new java.util.concurrent.locks.ReentrantLock();    
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
-    
+    public static final java.util.concurrent.Semaphore cpuSemaphore = new java.util.concurrent.Semaphore(1);
     // Method to increment context switch counter
     public static void incrementContextSwitch() {
         // TODO: Protect this critical section with a lock
